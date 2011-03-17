23fa7e0c595271da649c7b3d3bfc4d1cb40cd936
Karl Stolley <karl.stolley@gmail.com>
2011-03-17 13:24:24 -0500

    Test email form is now the live form

--- contact/index.php
+++ contact/index.php
@@ -32,9 +32,73 @@ swdb_header($swdbpage);
 
     <div id="main">
       <h2>Who Doesn't Like a Nice Email?</h2>
+      <?php
+        $to='karl.stolley@gmail.com';
+        $messageSubject='[SWD/Book] A Message about How to Design and Write Web Pages Today';
+        /*$confirmationSubject='Confirmation message subject';
+        $confirmationBody="Confirmation message body";*/
+        $email='';
+        $body='';
+        $displayForm=true;
+        if ($_POST){
+          $email=stripslashes($_POST['swde']);
+          $body=stripslashes($_POST['swdm']);
+          // validate e-mail address
+          $valid=eregi('^([0-9a-z]+[-._+&])*[0-9a-z]+@([-0-9a-z]+[.])+[a-z]{2,6}$',$email);
+          $crack=eregi("(\r|\n)(to:|from:|cc:|bcc:)",$body);
+          if ($email && $body && $valid && !$crack){
+            if (mail($to,$messageSubject,$body,'From: '.$email."\r\n")
+                /*&& mail($email,$confirmationSubject,$confirmationBody.$body,'From: '.$to."\r\n")*/){
+              $displayForm=false;
+      ?>
       <p>
-        Nice little form will go here.
+        Your message was sent just fine. I'll respond to you as soon as I can. Thanks!
+        <br />—Karl
+        <!--In addition, a confirmation copy was sent to your e-mail address.-->
       </p>
+      <?php
+              /*echo '<p>'.htmlspecialchars($body).'</p>';*/
+            }else{ // the messages could not be sent
+      ?>
+      <p>
+        Something went wrong when the server tried to send your message.
+        This is usually due to a server error, and is probably not your fault.
+        Send an email to karl.stolley@gmail.com if you'd like.
+      </p>
+      <?php
+            }
+          }else if ($crack){ // cracking attempt
+      ?>
+      <p><strong>
+        Your message contained e-mail headers within the message body.
+        This seems to be a cracking attempt and the message has not been sent.
+      </strong></p>
+      <?php
+          }else{ // form not complete
+      ?>
+      <p><strong>
+        Your message could not be sent.
+        You must include both a valid e-mail address and a message.
+      </strong></p>
+      <?php
+          }
+        }
+        if ($displayForm){
+      ?>
+      <form id="heydere" action="/book/contact/test.php" method="post">
+        <ul>
+          <li><label for="swde">Your e-mail address:</label>
+              <input type="text" name="swde" id="swde" value="<?php echo htmlspecialchars($email); ?>" size="30" />
+          </li>
+          <li><label for="swdm">Your message (make sure you put your name, too):</label>
+              <textarea name="swdm" id="swdm" cols="60" rows="8"><?php echo htmlspecialchars($body); ?></textarea>
+          </li>
+          <li><input type="submit" value="Send Message" /></li>
+        </ul>
+      </form>
+      <?php
+        }
+      ?>
     </div>
 
   <!--
