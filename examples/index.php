d80c896c17096e9adcbd3c531eece0e6674b0c42
Karl Stolley <karl.stolley@gmail.com>
2011-03-17 17:56:55 -0500

    Modified heading levels and styles for examples page/chapter names

--- examples/index.php
+++ examples/index.php
@@ -29,15 +29,14 @@ swdb_header($swdbpage);
 
     <div id="main">
       <!--NB: Glossary mentions additional terms to be found on the companion site-->
-      <h2>Part I. What Am I Writing?</h2>
       <div id="part-i">
-      <h3>Chapter 1: Why Write for the Web?</h3>
+      <h2>Chapter 1: Why Write for the Web?</h2>
         <div class="chapter">
-          <h4>Downloads and Utilities</h4>
+          <h3>Downloads and Utilities</h3>
           <ul>
             <li><a href="http://tweetdeck.com">TweetDeck</a> (Cross-platform social networking application)</li>
           </ul>
-          <h4>Websites</h4>
+          <h3>Websites</h3>
           <ul>
             <li><a href="http://www.onguardonline.gov/">On Guard Online</a> (Privacy and safety)</li>
             <li><a href="http://twitter.com/">Twitter</a> (Microblogging)</li>
@@ -51,9 +50,9 @@ swdb_header($swdbpage);
             <li><a href="http://www.viddler.com/">Viddler</a> (Video sharing)</li>
           </ul>
         </div>
-      <h3>Chapter 2: Reading the Web</h3>
+      <h2>Chapter 2: Reading the Web</h2>
         <div class="chapter">
-          <h4>Downloads and Utilities</h4>
+          <h3>Downloads and Utilities</h3>
           <ul>
             <li><a href="http://www.mozilla.com/firefox">Mozilla Firefox</a> (Open-source Web browser)</li>
             <li><a href="https://addons.mozilla.org/firefox/addon/60">Chris Pederick’s Web Developer Add-on for Firefox</a> (Firefox add-on)</li>
@@ -65,32 +64,32 @@ swdb_header($swdbpage);
             <li><a href="http://home.pacific.net.sg/~kennethkwok/lynx/">Lynx for Windows</a> (Text-only Web browser)</li>
             <li><a href="http://www.apple.com/downloads/macosx/unix_open_source/lynxtextwebbrowser.html">Lynx for Mac OS X</a> (Text-only Web browser)</li>
           </ul>
-          <h4>Websites</h4>
+          <h3>Websites</h3>
           <ul>
             <li>OLD WEBSITES OF MINE MENTIONED IN THE CHAPTER?</li>
           </ul>
         </div>
-      <h3>Chapter 3: Creating Web Content</h3>
+      <h2>Chapter 3: Creating Web Content</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://www.gimp.org">GIMP: GNU Image Manipulation Program</a> (Cross-platform free and open-source graphics software)</li>
           <li><a href="http://audacity.sourceforge.net">Audacity</a> (Cross-platform free and open-source audio software)</li>
           <li><a href="http://fixounet.free.fr/avidemux/">Avidemux</a> (Cross-platform free and open-source video software)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://www.blogarithms.com/index.php/mp3secrets/">The Secret Lives of MP3 Files</a> (Guidance in MP3 audio preparation)</li>
         </ul>
         </div>
-      <h3>Chapter 4: Standards-Based Web Pages</h3>
+      <h2>Chapter 4: Standards-Based Web Pages</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://validator.w3.org">W3C Markup Validation Service</a> (XHTML/HTML validator)</li>
           <li><a href="http://jigsaw.w3.org/cssvalidator/">W3C CSS Validation Service</a> (CSS validator)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://www.webpagesthatsuck.com/">Web Pages that Suck</a> (Gallery of bad design)</li>
           <li><a href="http://www.webstandards.org/">Web Standards Project (WaSP)</a> (Standards advocacy and education group)</li>
@@ -98,9 +97,9 @@ swdb_header($swdbpage);
           <li><a href="http://www.csszengarden.com/">CSS Zen Garden</a> (Site of CSS designs using the exact same XHTML)</li>
         </ul>
         </div>
-      <h3>Chapter 5: Preparing to Write and Design</h3>
+      <h2>Chapter 5: Preparing to Write and Design</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://portableapps.com">PortableApps.com</a> (Free and open-source Windows applications that can be run on a USB drive)</li>
           <li><a href="http://www.freesmug.org/portableapps/">FreeSMUG.org</a> (Free and open-source Mac OS X applications that can be run on a USB drive)</li>
@@ -122,12 +121,10 @@ swdb_header($swdbpage);
         </div>
       </div>
 
-      <h2>Part II. Issues and Challenges</h2>
-
       <div id="part-ii">
-      <h3>Chapter 6: Accessibility</h3>
+      <h2>Chapter 6: Accessibility</h2>
         <div class="chapter">
-        <h4>Websites</h4>
+        <h3>Websites</h3>
           <ul>
             <li><a href="http://www.w3.org/WAI/">Web Accessibility Initiative (WAI)</a> (W3C organization)</li>
             <li><a href="http://www.w3.org/WCAG/">Web Content Accessibility Guidelines (WCAG)</a> (W3C recommendation)</li>
@@ -136,41 +133,39 @@ swdb_header($swdbpage);
             <li><a href="http://access-board.gov/sec508/standards.htm">Section 508 guidelines and standards</a> (United States Access Board)</li>
           </ul>
         </div>
-      <h3>Chapter 7: Usability</h3>
+      <h2>Chapter 7: Usability</h2>
         <div class="chapter">
           <!--Nothing really in chapter, except for Nielsen stuff, which, blech. Bonus things here?-->
         </div>
-      <h3>Chapter 8: Sustainability</h3>
+      <h2>Chapter 8: Sustainability</h2>
         <div class="chapter">
           <!--Git stuff; mod_rewrite?-->
         </div>
       </div>
 
-      <h2>Part III. Strategies for Success</h2>
-
       <div id="part-iii">
-      <h3>Chapter 9: Structured Content: XHTML Overview</h3>
+      <h2>Chapter 9: Structured Content: XHTML Overview</h2>
         <div class="chapter">
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://htmldog.com/reference/htmltags/">HTML Dog HTML Tag Reference</a> (Language reference)</li>
           <li><a href="http://reference.sitepoint.com/html">SitePoint HTML Reference</a> (Language Reference)</li>
           <li><a href="http://microformats.org">Microformats.org</a> (Language reference)</li>
         </ul>
-        <h4>Examples</h4>
+        <h3>Examples</h3>
           <ul>
             <li>XHTML Strict DOCTYPE (p. 94)</li>
           </ul>
         </div>
-      <h3>Chapter 10: Presentation and Design: CSS Overview</h3>
+      <h2>Chapter 10: Presentation and Design: CSS Overview</h2>
         <div class="chapter">
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://htmldog.com/reference/cssproperties/">HTML Dog CSS Properties</a> (Language reference)</li>
           <li><a href="http://reference.sitepoint.com/css">SitePoint CSS Reference</a> (Language reference)</li>
           <li><a href="http://www.stuffandnonsense.co.uk/archives/css_specificity_wars.html">“CSS Specificity Wars”</a> (Blog post by Andy Clarke)</li>
         </ul>
-        <h4>Examples</h4>
+        <h3>Examples</h3>
         <ul>
           <li><a href="/book/examples/10/xhtml-sample.htm">A Sample of XHTML</a> (p. 104)</li>
           <li><a href="/book/examples/10/xhtml-css-sample.htm">A Sample of XHTML with a Cascade</a> (p. 104)</li>
@@ -179,49 +174,49 @@ swdb_header($swdbpage);
           <li><a href="/book/examples/10/hanging-indents.htm">Hanging Indents</a> (p. 115)</li>
         </ul>
         </div>
-      <h3>Chapter 11: Rapid Prototyping</h3>
+      <h2>Chapter 11: Rapid Prototyping</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="/book/rpk/">Rapid Prototyping Kit (RPK)</a></li>
           <li><a href="http://www.7-zip.org/download.html">7-Zip</a> (File archive program for Windows and other operating systems)</li>
           <li><a href="http://developer.yahoo.com/yui/reset/">Yahoo! YUI Reset CSS</a> (CSS reset file)</li>
           <li><a href="http://jquery.com">jQuery</a> (JavaScript library)</li>
         </ul>
-        <h4>Examples</h4>
+        <h3>Examples</h3>
         <ul>
           <li><a href="/book/rpk/htdocs/">Browsable RPK</a> (p. 124; download <a href="/book/rpk/">your own copy</a>)</li>
         </ul>
         </div>
-      <h3>Chapter 12: Writing with Source in a Text Editor</h3>
+      <h2>Chapter 12: Writing with Source in a Text Editor</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://notepad-plus-plus.org/">Notepad++ (“Notepad plus”)</a> (Recommended free text editor for Windows; can be run from USB drive)</li>
           <li><a href="http://www.barebones.com/products/textwrangler/">TextWrangler</a> (Recommended free text editor for Mac OS X; also <a href="http://itunes.apple.com/us/app/textwrangler/id404010395?mt=12">available via the Mac App Store</a>)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://en.wikipedia.org/wiki/Newline">Newline</a> (Wikipedia entry)</li>
         </ul>
-        <h4>Examples</h4>
+        <h3>Examples</h3>
         <ul>
           <li><a href="/book/examples/12/views/">One Page, Many Views</a> (p. 134-137)</li>
           <li><a href="/book/examples/12/views/highlighting-error.htm">Highlighting Error</a> (p. 137)</li>
           <li><a href="/book/examples/12/source-formatting.htm">Format Source However You'd Like</a> (p. 138-142)</li>
         </ul>
         </div>
-      <h3>Chapter 13: Page Metadata</h3>
+      <h2>Chapter 13: Page Metadata</h2>
         <div class="chapter">
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://webaim.org/techniques/screenreader/">“Designing for Screen Reader Compatibility”</a> (WebAIM article)</li>
           <li><a href="http://www.webmaster-toolkit.com/mime-types.shtml">MIME Types List</a> (Webmaster Toolkit reference)</li>
         </ul>
         </div>
-      <h3>Chapter 14: Page Branding</h3>
+      <h2>Chapter 14: Page Branding</h2>
         <div class="chapter">
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://www.mezzoblue.com/tests/revised-image-replacement/">Revised Image Replacement</a> (Dave Shea’s gallery of CSS image-replacement techniques)</li>
           <li><a href="http://validator.w3.org">W3C Markup Validation Service</a> (XHTML/HTML validator)</li>
@@ -229,17 +224,17 @@ swdb_header($swdbpage);
           <li><a href="http://creativecommons.org">Creative Commons</a> (Content licensing)</li>
         </ul>
         </div>
-      <h3>Chapter 15: Navigation</h3>
+      <h2>Chapter 15: Navigation</h2>
         <div class="chapter">
           <!--Nothing in the chapter, really.-->
         </div>
-      <h3>Chapter 16: Text Content</h3>
+      <h2>Chapter 16: Text Content</h2>
         <div class="chapter">
           <!--Nothing in the chapter, really.-->
         </div>
-      <h3>Chapter 17: Page Layout</h3>
+      <h2>Chapter 17: Page Layout</h2>
         <div class="chapter">
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://960.gs">960 Grid System</a> (Layout approaches)</li>
           <li><a href="http://978.gs">978 Grid System</a> (Layout approaches)</li>
@@ -247,27 +242,27 @@ swdb_header($swdbpage);
           <li><a href="http://msdn.microsoft.com/en-us/library/cc351024(VS.85).aspx">CSS Compatibility and Internet Explorer</a> (Internet Explorer work-arounds)</li>
         </ul>
         </div>
-      <h3>Chapter 18: Multimedia Content</h3>
+      <h2>Chapter 18: Multimedia Content</h2>
         <!--HTML5 stuff?-->
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://mediaplayer.yahoo.com/">Yahoo! Media Player</a> (Audio player)</li>
           <li><a href="http://wpaudioplayer.com/">WordPress Audio Player</a> (Audio player)</li>
           <li><a href="http://1bit.markwheeler.net/">1 Bit Audio Player</a> (Audio player)</li>
           <li><a href="http://code.google.com/p/swfobject/">SWFObject</a> (Standards-compliant Flash media loader)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://www.youtube.com/">YouTube</a> (Video sharing)</li>
           <li><a href="http://www.viddler.com/">Viddler</a> (Video sharing)</li>
           <li><a href="http://www.vimeo.com">Vimeo</a> (Video sharing)</li>
         </ul>
         </div>
-      <h3>Chapter 19: Performance and Interaction</h3>
+      <h2>Chapter 19: Performance and Interaction</h2>
         <div class="chapter">
           <!--Libraries under Downloads and Utilities?-->
-          <h4>Websites</h4>
+          <h3>Websites</h3>
           <ul>
             <li><a href="http://jquery.com">jQuery</a> (JavaScript library)</li>
             <li><a href="http://docs.jquery.com">jQuery Docs</a> (JavaScript library documentation)</li>
@@ -280,63 +275,61 @@ swdb_header($swdbpage);
         </div>
       </div>
 
-      <h2>Part IV. Problems and Solutions</h2>
-
       <div id="part-iv">
-      <h3>Chapter 20: Site Architecture</h3>
+      <h2>Chapter 20: Site Architecture</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://www.apachefriends.org/en/xampp-windows.html">XAMPP for Windows</a> (Development Web server)</li>
           <li><a href="http://www.apachefriends.org/en/xampp-macosx.html">XAMPP for Mac OS X</a> (Development Web server)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://www.apachefriends.org/en/faq-xampp.html">XAMPP</a> (Development Web server documentation)</li>
         </ul>
         </div>
-      <h3>Chapter 21: Reusing and Dynamically Generating Content</h3>
+      <h2>Chapter 21: Reusing and Dynamically Generating Content</h2>
         <div class="chapter">
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://php.net/docs.php">PHP documentation</a> (Language reference)</li>
         </ul>
         </div>
-      <h3>Chapter 22: Dynamic Sites in WordPress</h3>
+      <h2>Chapter 22: Dynamic Sites in WordPress</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://wordpress.org/download/">WordPress.org</a> (Blogging software)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://opensource.org/">Open Source Initiative</a> (Open source information)</li>
           <li><a href="http://wordpress.org/">WordPress.org</a> (Blogging software)</li>
           <li><a href="http://codex.wordpress.org/">Wordpress Codex</a> (WordPress documentation)</li>
         </ul>        </div>
-      <h3>Chapter 23: Going Live</h3>
+      <h2>Chapter 23: Going Live</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://winscp.net/">WinSCP</a> (FTP/SFTP client for Windows)</li>
           <li><a href="http://cyberduck.ch/">Cyberduck</a> (FTP/SFTP client for Mac OS X)</li>
           <li><a href="http://filezilla-project.org/">FileZilla</a> (FTP/SFTP client for all operating systems)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://no-www.org">no-www.org</a> (Site arguing against www. in Web URLs)</li>
           <li><a href="http://www.yes-www.org/">www.yes-www.org</a> (Site arguing for www. in Web URLs)</li>
         </ul>
       </div>
-      <h3>Chapter 24: Tracking Visitors, Sharing Content</h3>
+      <h2>Chapter 24: Tracking Visitors, Sharing Content</h2>
         <div class="chapter">
-        <h4>Downloads and Utilities</h4>
+        <h3>Downloads and Utilities</h3>
         <ul>
           <li><a href="http://www.mrunix.net/webalizer/">Webalizer</a> (Hosted site statistics)</li>
           <li><a href="http://www.labsmedia.com/clickheat/index.html">ClickHeat</a> (Hosted interaction tracking)</li>
           <li><a href="http://www.addthis.com/">AddThis</a> (JavaScript-based sharing button)</li>
         </ul>
-        <h4>Websites</h4>
+        <h3>Websites</h3>
         <ul>
           <li><a href="http://www.google.com/analytics/">Google Analytics</a> (Remote site statistics)</li>
           <li><a href="http://creativecommons.org">Creative Commons</a> (Content licensing)</li>
