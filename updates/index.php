19eea59b6780988abfb56e9f3fc3514c2543fad5
Karl Stolley <karl.stolley@gmail.com>
2011-03-25 11:55:43 -0500

    Added separate Updates/Corrections headings, and first correction :-(

--- updates/index.php
+++ updates/index.php
@@ -28,11 +28,17 @@ swdb_header($swdbpage);
   <div id="content">
 
     <div id="main">
-      <h2>Nothing Yet.</h2>
-      <p>
-        No updates or corrections yet. Found something? <a href="/book/contact/">Let the author
-        know</a>.
-      </p>
+      <h2>Updates</h2>
+      <ul>
+        <li>None yet. Have an update? <a href="/book/contact/">Let me know</a>.</li>
+      </ul>
+      <h2>Corrections</h2>
+      <ul>
+        <li><strong>Page 154</strong>: There should be no space before <code class="pp">icon</code> in the first <code class="pp">rel</code> example (the space between <code class="pp">shortcut icon</code> is correct, however). Here is the corrected version, with the correction highlighted:
+        <pre class="pp">&lt;link rel=<strong>&quot;icon&quot;</strong> href=&quot;http://example.com/favicon.png&quot; type=&quot;image/png&quot; /&gt;&#10;&lt;!--For Internet Explorer; no type must be specified--&gt;&#10;&lt;link rel=&quot;shortcut icon&quot; href=&quot;http://example.com/favicon.ico&quot; /&gt;</pre>
+        </li>
+        <li>That’s all for now. Find a mistake that needs to be corrected? <a href="/book/contact/">Let me know</a>.</li>
+      </ul>
     </div>
 
   <!--
