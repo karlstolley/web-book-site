1040d97b56c9672da997d1e4df21c2233890777b
Karl Stolley <karl.stolley@gmail.com>
2011-04-18 15:59:16 -0500

    Added corrections for p. 244, p. 39

--- updates/index.php
+++ updates/index.php
@@ -34,8 +34,13 @@ swdb_header($swdbpage);
       </ul>
       <h2>Corrections</h2>
       <ul>
+        <li><strong>Page 39</strong>: What used to be the European Computer Manufacturers Association goes by <a href="http://www.ecma-international.org/">Ecma International</a> (Ecma, not ECMA).</li>
         <li><strong>Page 154</strong>: There should be no space before <code class="pp">icon</code> in the first <code class="pp">rel</code> example (the space between <code class="pp">shortcut icon</code> is correct, however). Here is the corrected version, with the correction highlighted:
         <pre class="pp">&lt;link rel=<strong>&quot;icon&quot;</strong> href=&quot;http://example.com/favicon.png&quot; type=&quot;image/png&quot; /&gt;&#10;&lt;!--For Internet Explorer; no type must be specified--&gt;&#10;&lt;link rel=&quot;shortcut icon&quot; href=&quot;http://example.com/favicon.ico&quot; /&gt;</pre>
+        </li>
+        <li><strong>Page 244</strong>: The colon (<code>:</code>) and two slashes (<code>/</code>) must be escaped with two back-slashes (<code>\\</code>) in the attribute selector, <a href="http://api.jquery.com/category/selectors/">per jQuery’s documentation</a>. Here is the correct selector for hitting external links inside of <code>div#content</code>, with the corrections highlighted::
+        <pre class="pp lang-js">/*JavaScript inside the ready event*/
+$('div#content a[href^=http<strong>\\:\\/\\/</strong>]').addClass('ext');</pre>
         </li>
         <li>That’s all for now. Find a mistake that needs to be corrected? <a href="/book/contact/">Let me know</a>.</li>
       </ul>
